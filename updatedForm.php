37cf505625573db3bcde20fdf8cdcbc629354c06
sheikh munasib <seropask123@gmail.com>
2026-06-29 12:32:35 +0600

    Add files via upload

---
+++ updatedForm.php
@@ -0,0 +1,193 @@
+<html>
+
+<head>
+    <title>Registration</title>
+</head>
+
+<body>
+
+    <h1 align="center">Registration</h1>
+
+    <form>
+
+        <fieldset>
+            <legend><b>Registration Form</b></legend>
+
+            <table width="100%" cellpadding="10">
+                <tr>
+
+                    <!-- General Information -->
+                    <td valign="top" width="35%">
+                        <fieldset>
+                            <legend><b>General Information</b></legend>
+
+                            <table>
+                                <tr>
+                                    <td>First Name</td>
+                                    <td>: <input type="text"></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Last Name</td>
+                                    <td>: <input type="text"></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Gender</td>
+                                    <td>
+                                        : <input type="radio" name="gender"> Male
+                                        <input type="radio" name="gender"> Female
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>Father's Name</td>
+                                    <td>
+                                        : <input type="text">
+                                        <input type="file">
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>Mother's Name</td>
+                                    <td>: <input type="text"></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Blood Group</td>
+                                    <td>
+                                        : <select>
+                                            <option>A+</option>
+                                            <option>A-</option>
+                                            <option>B+</option>
+                                            <option>B-</option>
+                                            <option>AB+</option>
+                                            <option>AB-</option>
+                                            <option>O+</option>
+                                            <option>O-</option>
+                                        </select>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>Religion</td>
+                                    <td>
+                                        : <select>
+                                            <option>Islam</option>
+                                            <option>Hindu</option>
+                                            <option>Christian</option>
+                                            <option>Buddhist</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                            </table>
+
+                        </fieldset>
+                    </td>
+
+                    <!-- Contact Information -->
+                    <td valign="top" width="35%">
+                        <fieldset>
+                            <legend><b>Contact Information</b></legend>
+
+                            <table>
+                                <tr>
+                                    <td>Email</td>
+                                    <td>: <input type="email"></td>
+
+                                </tr>
+
+                                <tr>
+                                    <td>Phone/Mobile</td>
+                                    <td>: <input type="text"></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Website</td>
+                                    <td>: <input type="text"></td>
+                                </tr>
+
+
+                                <tr>
+
+
+                                <tr>
+                                    <td>Address :</td>
+                                    <td>
+                                        <textarea rows="4" cols="20"></textarea>
+                                    </td>
+                                </tr>
+                                <td>
+                                    <select>
+                                        <option>Bangladesh</option>
+                                        <option>USA</option>
+                                        <option>Brazil</option>
+                                    </select>
+                                </td>
+                </tr>
+
+                <tr>
+
+                    <td>
+                        <select>
+                            <option>Dhaka</option>
+                            <option>NewYork</option>
+                        </select>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>Post Code :</td>
+                    <td><input type="text"></td>
+                </tr>
+            </table>
+
+        </fieldset>
+        </td>
+
+        <!-- Account Information -->
+        <td valign="top" width="30%">
+            <fieldset>
+                <legend><b>Account Information</b></legend>
+
+                <table>
+                    <tr>
+                        <td>Username</td>
+                        <td>: <input type="text"></td>
+                    </tr>
+
+                    <tr>
+                        <td>Password</td>
+                        <td>: <input type="password"></td>
+                    </tr>
+
+                    <tr>
+                        <td>Confirm Password</td>
+                        <td>: <input type="password"></td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="2" align="center">
+
+
+
+                        </td>
+                    </tr>
+                </table>
+
+            </fieldset>
+            <br>
+            <input type="submit" value="Register">
+        </td>
+
+        </tr>
+        </table>
+
+        </fieldset>
+
+    </form>
+
+</body>
+
+</html>
